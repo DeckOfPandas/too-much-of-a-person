9b2444e0ae571b9ea138590f9ef130dd826b2456
Helen Jackson <helenj@ckson.io>
2019-02-21 09:44:57 +0000

    Base Swiper with thumbnail slides

--- _assets/scripts/global/tmoap.js
+++ _assets/scripts/global/tmoap.js
@@ -21,3 +21,23 @@
   });
 
 })(jQuery); // End of use strict
+
+
+// Initialise Swiper
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+      spaceBetween: 10,
+      slidesPerView: 3,
+      freeMode: true,
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
+    });
+    var galleryTop = new Swiper('.gallery-top', {
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      thumbs: {
+        swiper: galleryThumbs
+      }
+    });
